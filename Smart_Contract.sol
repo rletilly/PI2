631c141c53116bf75d312ea477e93d4b44ba931c
Tfrx1er <theophile.freixa@outlook.fr>
2020-02-03 14:29:13 +0100

    v1 smart contract
    Ne fonctionne pas encore

--- Smart_Contract.sol
+++ Smart_Contract.sol
@@ -1,1 +1,35 @@
-pragma solidity 
+pragma solidity ^0.5.0;
+
+contract CoinaAppart {
+    address public _locataire;
+    address public _proprietaire;
+    
+    bool public _validationLocataire;
+    bool public _validationProprietaire;
+    
+    uint24 public amount;
+    
+    bytes32 public hashDoc;
+    
+    constructor(address proprietaire) public 
+    {
+        _locataire = msg.sender;
+        _proprietaire = proprietaire;
+    }
+    
+    
+    function launchContract(bytes32  hash, uint24  amount ) public returns (bool check)
+    {
+        if (_validationLocataire || _validationProprietaire)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+    
+    bool public _launchContract;
+    
+}
